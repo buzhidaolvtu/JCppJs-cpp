f75df2660982211f031e40439248277f0440e223
吕途 <buzhidaolvtu@163.com>
2017-11-06 11:40:24 +0800

    Initial Commit

---
+++ v8-hello-world/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  v8-hello-world
+//
+//  Created by 吕途 on 2017/11/6.
+//  Copyright © 2017年 吕途. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
